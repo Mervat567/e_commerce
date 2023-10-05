ba636e8e2995ee26fe55524366b8c3c052197882
mervat <mervatgomaa34@gmail.com>
2023-10-05 19:24:27 +0300

    database folder

--- config/app.js
+++ config/app.js
@@ -1,7 +1,4 @@
 const express=require("express")
 const app=express()
-require("dotenv").config()
-const connection=require('./database').connection
-connection()
 
 module.exports=app
--- config/server.js
+++ config/server.js
@@ -1,8 +1,18 @@
 const app=require('./app')
 require("dotenv").config()
+const mongoose=require('mongoose')
 
-app.listen(process.env.PORT,()=>{
- console.log(`server is up on port ${process.env.PORT}`)
-})
+const connection=async()=>{
+    try{
+    await mongoose.connect(process.env.URI)
+     console.log('connected to database successfully') 
+     app.listen(process.env.PORT,()=>{
+        console.log(`server is up on port ${process.env.PORT}`)
+       })  
+    }catch(err){
+     console.log("MongoDB error :" +err)
+    }
+}
 
+connection()
 module.exports=app
